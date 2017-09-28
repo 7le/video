5510a58a7e0d1056b8f65d144cf5309f7a8e391d
7le <747037402@qq.com>
2017-09-28 13:45:16 +0800

    添加SpringBeanUtil 直接通过Spring上下文获取SpringBean

---
+++ src/main/java/com/shine/video/util/SpringBeanUtil.java
@@ -0,0 +1,36 @@
+package com.shine.video.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hq
+ * @Description: 直接通过Spring上下文获取SpringBean
+ * @date 2017年8月17日
+ * @since v4.19.0
+ */
+@Component
+public class SpringBeanUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringBeanUtil.applicationContext = applicationContext;
+    }
+
+    public static Object getBeanByName(String beanName) {
+        if (applicationContext == null){
+            return null;
+        }
+        return applicationContext.getBean(beanName);
+    }
+
+    public static <T> T getBean(Class<T> type) {
+        return applicationContext.getBean(type);
+    }
+
+}
